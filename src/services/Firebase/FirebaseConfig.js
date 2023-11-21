6d4b20d4a64168f95e15a494b8c8bd5abee6d80c
FinochioAdrian <eidrienhez33@gmail.com>
2023-11-21 12:02:07 -0300

    Add FirebaseConfig

--- src/services/Firebase/FirebaseConfig.js
+++ src/services/Firebase/FirebaseConfig.js
@@ -1,6 +1,5 @@
-import { getFirestore } from "@firebase/firestore";
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAeu4DR5Jm5hbw3j70Wq_-yHXsBtH0YTS0",
@@ -8,10 +7,8 @@ const firebaseConfig = {
   projectId: "web-design-ideas-snowfox",
   storageBucket: "web-design-ideas-snowfox.appspot.com",
   messagingSenderId: "98449105268",
-  appId: "1:98449105268:web:61bfd0315a5c0d3acb8c21"
+  appId: "1:98449105268:web:61bfd0315a5c0d3acb8c21",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app)
+export const db = getFirestore(app);
